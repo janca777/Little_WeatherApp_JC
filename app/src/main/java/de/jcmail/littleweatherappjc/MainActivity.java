f05903c05fd2467b044fc9de3ecde05e7bf54e2d
J. Carstensen <familie.carstensen@gmail.com>
2019-12-11 10:16:11 +0100

    GUI Layout design. Loopj dependency for network communication added.

--- app/src/main/java/de/jcmail/littleweatherappjc/MainActivity.java
+++ app/src/main/java/de/jcmail/littleweatherappjc/MainActivity.java
@@ -1,14 +1,35 @@
 package de.jcmail.littleweatherappjc;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    //variables for the layout
+    private TextView dateView;
+    private TextView locationTextView;
+    private TextView tempTextView;
+    private TextView weatherDescritptionTextView;
+
+
+    private String myTemperature;
+    private String myCity;
+    private String myWeatherDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateView = findViewById(R.id.dateView);
+        locationTextView = findViewById(R.id.locationTextView);
+        tempTextView = findViewById(R.id.tempTextView);
+        weatherDescritptionTextView = findViewById(R.id.weatherDescriptionTextView);
+
+
+
+
     }
 }
